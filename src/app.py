6c881805a5880768410c02964138ffb11f07e366
rock <javaxxl@163.com>
2025-10-15 17:11:04 +0800

    rm -rf workspace in jenkins

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     # Gets the hostname of the pod
     pod_name = os.environ.get('HOSTNAME', 'unknown')
-    return f"Hello from Pod: {pod_name}!\n great！"
+    return f"Hello from Pod: {pod_name}!\n great     ！"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
